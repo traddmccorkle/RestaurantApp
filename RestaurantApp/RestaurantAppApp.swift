99e8c395f5cf95f913baf361b58efa3bd4b35179
Tradd McCorkle <tradd92@gmail.com>
2023-12-10 22:11:07 -0500

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Tradd McCorkle on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
